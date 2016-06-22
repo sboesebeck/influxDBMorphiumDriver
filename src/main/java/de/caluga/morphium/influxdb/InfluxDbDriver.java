2f41d1597c705372a8d61ec1aaaaf97d8709bd71
Stephan Bösebeck <sb@caluga.de>
2016-06-22 16:26:12 +0200

    writing works

--- src/main/java/de/caluga/morphium/influxdb/InfluxDbDriver.java
+++ src/main/java/de/caluga/morphium/influxdb/InfluxDbDriver.java
@@ -3,21 +3,32 @@ package de.caluga.morphium.influxdb;
 import de.caluga.morphium.Logger;
 import de.caluga.morphium.Morphium;
 import de.caluga.morphium.driver.*;
+import de.caluga.morphium.driver.bson.MorphiumId;
 import de.caluga.morphium.driver.bulk.BulkRequestContext;
 import de.caluga.morphium.driver.mongodb.Maximums;
-import org.apache.http.HttpClientConnection;
-import org.apache.http.HttpEntity;
+import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.DefaultManagedHttpClientConnection;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -30,9 +41,28 @@ public class InfluxDbDriver implements MorphiumDriver {
     private int conTimeout=5000;
     private int heartbeatFrequency=1000;
     private Logger log=new Logger(InfluxDbDriver.class);
-    private PoolingHttpClientConnectionManager conMgr=new PoolingHttpClientConnectionManager(getMaxConnectionLifetime(), TimeUnit.MILLISECONDS);
+    private PoolingHttpClientConnectionManager conMgr;
 
-    public void setCredentials(String s, String s1, char[] chars) {
+    private String[] hosts;
+
+    private ConnectionKeepAliveStrategy keepAliveStrategy = new ConnectionKeepAliveStrategy() {
+        public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+            HeaderElementIterator it = new BasicHeaderElementIterator
+                    (response.headerIterator(HTTP.CONN_KEEP_ALIVE));
+            while (it.hasNext()) {
+                HeaderElement he = it.nextElement();
+                String param = he.getName();
+                String value = he.getValue();
+                if (value != null && param.equalsIgnoreCase
+                        ("timeout")) {
+                    return Long.parseLong(value) * 1000;
+                }
+            }
+            return 5 * 1000;
+        }
+    };
+
+    public void setCredentials(String db, String login, char[] pwd) {
 
     }
 
@@ -40,7 +70,7 @@ public class InfluxDbDriver implements MorphiumDriver {
         return false;
     }
 
-    public String[] getCredentials(String s) {
+    public String[] getCredentials(String db) {
         return new String[0];
     }
 
@@ -49,11 +79,11 @@ public class InfluxDbDriver implements MorphiumDriver {
     }
 
     public String[] getHostSeed() {
-        return new String[0];
+        return hosts;
     }
 
     public int getMaxConnectionsPerHost() {
-        return 0;
+        return maxConPerHost;
     }
 
     public int getMinConnectionsPerHost() {
@@ -88,27 +118,27 @@ public class InfluxDbDriver implements MorphiumDriver {
         return 0;
     }
 
-    public void setHeartbeatSocketTimeout(int i) {
+    public void setHeartbeatSocketTimeout(int heartbeatSocketTimeout) {
 
     }
 
-    public void setUseSSL(boolean b) {
+    public void setUseSSL(boolean useSSL) {
 
     }
 
-    public void setHeartbeatFrequency(int i) {
+    public void setHeartbeatFrequency(int heartbeatFrequency) {
 
     }
 
-    public void setWriteTimeout(int i) {
+    public void setWriteTimeout(int writeTimeout) {
 
     }
 
-    public void setDefaultBatchSize(int i) {
+    public void setDefaultBatchSize(int defaultBatchSize) {
 
     }
 
-    public void setCredentials(Map<String, String[]> map) {
+    public void setCredentials(Map<String, String[]> credentials) {
 
     }
 
@@ -132,66 +162,66 @@ public class InfluxDbDriver implements MorphiumDriver {
         return 0;
     }
 
-    public void setHostSeed(String... strings) {
+    public void setHostSeed(String... host) {
+        hosts=host;
+    }
+
+    public void setMaxConnectionsPerHost(int mx) {
 
     }
 
-    public void setMaxConnectionsPerHost(int i) {
+    public void setMinConnectionsPerHost(int mx) {
 
     }
 
-    public void setMinConnectionsPerHost(int i) {
+    public void setMaxConnectionLifetime(int timeout) {
 
     }
 
-    public void setMaxConnectionLifetime(int i) {
+    public void setMaxConnectionIdleTime(int time) {
 
     }
 
-    public void setMaxConnectionIdleTime(int i) {
+    public void setSocketTimeout(int timeout) {
 
     }
 
-    public void setSocketTimeout(int i) {
+    public void setConnectionTimeout(int timeout) {
 
     }
 
-    public void setConnectionTimeout(int i) {
+    public void setDefaultW(int w) {
 
     }
 
-    public void setDefaultW(int i) {
+    public void setMaxBlockingThreadMultiplier(int m) {
 
     }
 
-    public void setMaxBlockingThreadMultiplier(int i) {
+    public void heartBeatFrequency(int t) {
 
     }
 
-    public void heartBeatFrequency(int i) {
+    public void heartBeatSocketTimeout(int t) {
 
     }
 
-    public void heartBeatSocketTimeout(int i) {
-
-    }
-
-    public void useSsl(boolean b) {
+    public void useSsl(boolean ssl) {
 
     }
 
     public void connect() throws MorphiumDriverException {
+        conMgr=new PoolingHttpClientConnectionManager(getMaxConnectionLifetime(),TimeUnit.MILLISECONDS);
+        conMgr.setDefaultMaxPerRoute(100);
+        conMgr.setMaxTotal(100000);
+    }
+
+    public void setDefaultReadPreference(ReadPreference rp) {
 
     }
 
-    public void setDefaultReadPreference(ReadPreference readPreference) {
-
-    }
-
-    public void connect(String s) throws MorphiumDriverException {
-        //replicaset is ignored
-
-
+    public void connect(String replicasetName) throws MorphiumDriverException {
+        connect();
     }
 
     public Maximums getMaximums() {
@@ -202,11 +232,11 @@ public class InfluxDbDriver implements MorphiumDriver {
         return false;
     }
 
-    public void setDefaultJ(boolean b) {
+    public void setDefaultJ(boolean j) {
 
     }
 
-    public void setDefaultWriteTimeout(int i) {
+    public void setDefaultWriteTimeout(int wt) {
 
     }
 
@@ -214,15 +244,15 @@ public class InfluxDbDriver implements MorphiumDriver {
         return 0;
     }
 
-    public void setLocalThreshold(int i) {
+    public void setLocalThreshold(int thr) {
 
     }
 
-    public void setDefaultFsync(boolean b) {
+    public void setDefaultFsync(boolean j) {
 
     }
 
-    public void setRetriesOnNetworkError(int i) {
+    public void setRetriesOnNetworkError(int r) {
 
     }
 
@@ -230,7 +260,7 @@ public class InfluxDbDriver implements MorphiumDriver {
         return 0;
     }
 
-    public void setSleepBetweenErrorRetries(int i) {
+    public void setSleepBetweenErrorRetries(int s) {
 
     }
 
@@ -246,95 +276,156 @@ public class InfluxDbDriver implements MorphiumDriver {
         return null;
     }
 
-    public Map<String, Object> getDBStats(String s) throws MorphiumDriverException {
+    public Map<String, Object> getDBStats(String db) throws MorphiumDriverException {
         return null;
     }
 
-    public Map<String, Object> getOps(long l) throws MorphiumDriverException {
+    public Map<String, Object> getOps(long threshold) throws MorphiumDriverException {
         return null;
     }
 
-    public Map<String, Object> runCommand(String s, Map<String, Object> map) throws MorphiumDriverException {
+    public Map<String, Object> runCommand(String db, Map<String, Object> cmd) throws MorphiumDriverException {
         return null;
     }
 
-    public MorphiumCursor initIteration(String s, String s1, Map<String, Object> map, Map<String, Integer> map1, Map<String, Object> map2, int i, int i1, int i2, ReadPreference readPreference, Map<String, Object> map3) throws MorphiumDriverException {
+    public MorphiumCursor initIteration(String db, String collection, Map<String, Object> query, Map<String, Integer> sort, Map<String, Object> projection, int skip, int limit, int batchSize, ReadPreference readPreference, Map<String, Object> findMetaData) throws MorphiumDriverException {
         return null;
     }
 
-    public MorphiumCursor nextIteration(MorphiumCursor morphiumCursor) throws MorphiumDriverException {
+    public MorphiumCursor nextIteration(MorphiumCursor crs) throws MorphiumDriverException {
         return null;
     }
 
-    public void closeIteration(MorphiumCursor morphiumCursor) throws MorphiumDriverException {
+    public void closeIteration(MorphiumCursor crs) throws MorphiumDriverException {
 
     }
 
-    public List<Map<String, Object>> find(String s, String s1, Map<String, Object> map, Map<String, Integer> map1, Map<String, Object> map2, int i, int i1, int i2, ReadPreference readPreference, Map<String, Object> map3) throws MorphiumDriverException {
+    public List<Map<String, Object>> find(String db, String collection, Map<String, Object> query, Map<String, Integer> sort, Map<String, Object> projection, int skip, int limit, int batchSize, ReadPreference rp, Map<String, Object> findMetaData) throws MorphiumDriverException {
         return null;
     }
 
-    public long count(String s, String s1, Map<String, Object> map, ReadPreference readPreference) throws MorphiumDriverException {
+    public long count(String db, String collection, Map<String, Object> query, ReadPreference rp) throws MorphiumDriverException {
         return 0;
     }
 
-    public void insert(String s, String s1, List<Map<String, Object>> list, WriteConcern writeConcern) throws MorphiumDriverException {
-
+    public void insert(String db, String collection, List<Map<String, Object>> objs, WriteConcern wc) throws MorphiumDriverException {
+        store(db,collection,objs,wc);
     }
 
-    public void store(String s, String s1, List<Map<String, Object>> list, WriteConcern writeConcern) throws MorphiumDriverException {
-        CloseableHttpClient cl= HttpClients.createDefault();
-        HttpPost p=new HttpPost("http://localhost:8086/write?db=graphite");
-        StringEntity e=new StringEntity("cpu_load_short,host=server01,region=us-west value=0.64 "+System.currentTimeMillis(),"UTF8");
-        p.setEntity(e);
-        try {
-            cl.execute(p);
-        } catch (IOException e1) {
-            e1.printStackTrace();
+    public void store(String db, String collection, List<Map<String, Object>> list, WriteConcern writeConcern) throws MorphiumDriverException {
+
+        StringBuilder b = new StringBuilder();
+        b.append(collection).append(",");
+
+        //assuming number and boolean values as influx values
+        //all others as tags
+
+        for (Map<String, Object> measurement : list) {
+            long tm = System.nanoTime();
+
+
+            if (measurement.get("_id") != null) {
+//                log.warn("Cannot up´date values in influxdb! Will create a new entry!");
+                try {
+                    tm = Long.valueOf(measurement.get("_id").toString());
+                } catch (NumberFormatException e) {
+                    log.warn("could not read timestamp from _id field! Assuming now!");
+                }
+            }
+
+
+            List<String> valueKeys = new ArrayList<String>();
+            for (Map.Entry<String, Object> entry : measurement.entrySet()) {
+
+                if (entry.getKey().equals("_id")) continue;
+                if (entry.getValue() instanceof Number || entry.getValue() instanceof Double || entry.getValue() instanceof Float || entry.getValue() instanceof Integer || entry.getValue() instanceof Long || entry.getValue() instanceof Boolean) {
+                    valueKeys.add(entry.getKey());
+                    continue;
+                }
+                try {
+                    b.append(entry.getKey()).append("=").append(URLEncoder.encode(entry.getValue().toString(),"UTF8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                b.append(",");
+            }
+
+            b.setLength(b.length() - 1);
+            b.append(" ");
+            for (String v : valueKeys) {
+                try {
+                    b.append(v).append("=").append(URLEncoder.encode(measurement.get(v).toString(),"UTF8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                b.append(",");
+            }
+            b.setLength(b.length() - 1);
+            b.append(" ").append(tm);
+            b.append("\n");
+            measurement.put("_id", tm);
+        }
+        CloseableHttpClient cl = HttpClients.custom().setKeepAliveStrategy(keepAliveStrategy).
+                setConnectionManager(conMgr).build();
+
+        //need to write to all hosts in cluster
+        for (String h: getHostSeed()) {
+            HttpPost p = new HttpPost("http://"+h+"/write?db=" + db);
+
+            StringEntity e = new StringEntity(b.toString(), "UTF8");
+            log.info("Sending to db " + db + " on host "+h+": " + b.toString());
+            p.setEntity(e);
+            try {
+                cl.execute(p).close();
+
+            } catch (IOException e1) {
+                throw new MorphiumDriverException("ioexception",e1);
+            }
         }
     }
 
     public Map<String, Object> update(String s, String s1, Map<String, Object> map, Map<String, Object> map1, boolean b, boolean b1, WriteConcern writeConcern) throws MorphiumDriverException {
+        log.error("Cannot run updates on influxdb");
         return null;
     }
 
-    public Map<String, Object> delete(String s, String s1, Map<String, Object> map, boolean b, WriteConcern writeConcern) throws MorphiumDriverException {
+    public Map<String, Object> delete(String db, String collection, Map<String, Object> query, boolean multiple, WriteConcern wc) throws MorphiumDriverException {
         return null;
     }
 
-    public void drop(String s, String s1, WriteConcern writeConcern) throws MorphiumDriverException {
+    public void drop(String db, String collection, WriteConcern wc) throws MorphiumDriverException {
 
     }
 
-    public void drop(String s, WriteConcern writeConcern) throws MorphiumDriverException {
+    public void drop(String db, WriteConcern wc) throws MorphiumDriverException {
 
     }
 
-    public boolean exists(String s) throws MorphiumDriverException {
+    public boolean exists(String db) throws MorphiumDriverException {
         return false;
     }
 
-    public List<Object> distinct(String s, String s1, String s2, Map<String, Object> map, ReadPreference readPreference) throws MorphiumDriverException {
+    public List<Object> distinct(String db, String collection, String field, Map<String, Object> filter, ReadPreference rp) throws MorphiumDriverException {
         return null;
     }
 
-    public boolean exists(String s, String s1) throws MorphiumDriverException {
+    public boolean exists(String db, String collection) throws MorphiumDriverException {
         return false;
     }
 
-    public List<Map<String, Object>> getIndexes(String s, String s1) throws MorphiumDriverException {
+    public List<Map<String, Object>> getIndexes(String db, String collection) throws MorphiumDriverException {
         return null;
     }
 
-    public List<String> getCollectionNames(String s) throws MorphiumDriverException {
+    public List<String> getCollectionNames(String db) throws MorphiumDriverException {
         return null;
     }
 
-    public Map<String, Object> group(String s, String s1, Map<String, Object> map, Map<String, Object> map1, String s2, String s3, ReadPreference readPreference, String... strings) throws MorphiumDriverException {
+    public Map<String, Object> group(String db, String coll, Map<String, Object> query, Map<String, Object> initial, String jsReduce, String jsFinalize, ReadPreference rp, String... keys) throws MorphiumDriverException {
         return null;
     }
 
-    public List<Map<String, Object>> aggregate(String s, String s1, List<Map<String, Object>> list, boolean b, boolean b1, ReadPreference readPreference) throws MorphiumDriverException {
+    public List<Map<String, Object>> aggregate(String db, String collection, List<Map<String, Object>> pipeline, boolean explain, boolean allowDiskUse, ReadPreference readPreference) throws MorphiumDriverException {
         return null;
     }
 
@@ -342,7 +433,7 @@ public class InfluxDbDriver implements MorphiumDriver {
         return false;
     }
 
-    public void setSocketKeepAlive(boolean b) {
+    public void setSocketKeepAlive(boolean socketKeepAlive) {
 
     }
 
@@ -350,7 +441,7 @@ public class InfluxDbDriver implements MorphiumDriver {
         return 0;
     }
 
-    public void setHeartbeatConnectTimeout(int i) {
+    public void setHeartbeatConnectTimeout(int heartbeatConnectTimeout) {
 
     }
 
@@ -358,19 +449,22 @@ public class InfluxDbDriver implements MorphiumDriver {
         return 0;
     }
 
-    public void setMaxWaitTime(int i) {
+    public void setMaxWaitTime(int maxWaitTime) {
 
     }
 
-    public boolean isCapped(String s, String s1) throws MorphiumDriverException {
+    public boolean isCapped(String db, String coll) throws MorphiumDriverException {
         return false;
     }
 
-    public BulkRequestContext createBulkContext(Morphium morphium, String s, String s1, boolean b, WriteConcern writeConcern) {
+    public BulkRequestContext createBulkContext(Morphium m, String db, String collection, boolean ordered, WriteConcern wc) {
         return null;
     }
 
-    public void createIndex(String s, String s1, Map<String, Object> map, Map<String, Object> map1) throws MorphiumDriverException {
+    public void createIndex(String db, String collection, Map<String, Object> index, Map<String, Object> options) throws MorphiumDriverException {
 
     }
+
+
+
 }
--- src/test/java/de/caluga/test/influxdbdriver/BasicTest.java
+++ src/test/java/de/caluga/test/influxdbdriver/BasicTest.java
@@ -14,19 +14,61 @@ import org.junit.Test;
 public class BasicTest {
 
     @Test
-    public void basicConnectionTest() throws Exception{
-        MorphiumConfig cfg=new MorphiumConfig("graphite",100,1000,10000);
+    public void basicConnectionTest() throws Exception {
+        String hosts[] = new String[]{"host_1", "host_2", "host_3"};
+        String urls[] = new String[]{"/", "/checkout/new", "/search/filter"};
+        String search[] = new String[]{"bohrer", "schmiermittel", "elastic", "item", "usb-hub", "Apple MacBook Pro 15"};
+        String session[] = new String[]{"12345", "12333", "12328", "Session32", "123dsl", "affe", "deadbeef"};
+        int ret[] = new int[]{200, 203, 301, 302, 403, 400, 500};
+        MorphiumConfig cfg = new MorphiumConfig("graphite", 100, 1000, 10000);
         cfg.setDriverClass(InfluxDbDriver.class.getName());
         cfg.addHostToSeed("localhost:8086");
-        Morphium m=new Morphium(cfg);
+        cfg.setDatabase("graphite");
 
-        m.store(new EntTest());
+        cfg.setLogLevelForClass(InfluxDbDriver.class, 5);
+        cfg.setGlobalLogSynced(true);
+        Morphium m = new Morphium(cfg);
+
+        for (int i = 0; i < 10000; i++) {
+            EntTest e = new EntTest();
+            e.id = (long) (System.currentTimeMillis() * 1000 * 1000 - Math.random() * 1000 * 1000 * 1000 * 600);
+
+
+            e.host = hosts[(int) (Math.random() * hosts.length)];
+            e.reqtime = 500 * Math.random() + 100 + i;
+            e.lfd = i;
+            e.success = Math.random() > 0.5;
+            if (Math.random() > 0.5) {
+                e.isSearch = true;
+            } else {
+                e.isSearch = false;
+                e.search = search[(int) (Math.random() * search.length)];
+            }
+            e.retCode = ret[(int) (Math.random() * ret.length)];
+            e.sizekb = 100 * (1.0 + Math.random());
+            e.url = urls[(int) (Math.random() * urls.length)];
+            e.sessionid=session[(int) (session.length*Math.random())];
+            if (e.url.contains("checkout")){
+                e.checkoutAmount =1000000*Math.random();
+            }
+            m.store(e);
+        }
     }
 
-    @Entity
+    @Entity(collectionName = "requests")
     public static class EntTest {
         @Id
-        private String id;
-        private double value=123.345;
+        public Long id;
+        public String host = "myHost";
+        public String sessionid = "sessionid";
+        public int lfd = 123455;
+        public double reqtime = 123.345;
+        public boolean success = true;
+        public String url = "";
+        public int retCode = 404;
+        public double sizekb = 0;
+        public String search = "";
+        public boolean isSearch = false;
+        public double checkoutAmount =0;
     }
 }
